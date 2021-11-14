6ae87c021f0caff92aa9f752f2226d1abdaf8206
Damyyr <maxime.damour974@gmail.com>
2021-11-13 23:13:02 -0500

    feat(broker): add a simplistic functionnal broker

--- broker/app.js
+++ broker/app.js
@@ -1,14 +1,36 @@
-const { Console } = require('console')
-
+const appCreds = require('./credentials.js').appCreds
 const aedes = require('aedes')()
 const server = require('net').createServer(aedes.handle)
-const port = 5000
 
-server.listen(port, function () {
-  console.log('server started and listening on port ', port)
+// const cron = require('node-cron')
+// const tasks = []; // TODO: move this in a module like "tasksManager" or smthg
+// Usage: tasks.push || tasks.foreach() -> access all the registred ongoing tasks
+
+server.listen(appCreds.aedesPort, function () {
+  console.log('server started and listening on port ', appCreds.aedesPort)
+})
+
+server.on('connect', () => {
+  console.log('connect')
+})
+
+server.on('message', (topic, message) => {
+  console.log(`topic: ${topic} | message: ${message}`)
+})
+
+server.on('hm', (topic, message) => {
+  console.log(`topic: ${topic} | message: ${message}`)
+  aedes.publish('hm', 'someone just quit')
+})
+
+server.on('error', (err) => {
+  console.error('ERROR: ', err)
 })
 
 aedes.on('subscribe', async function (packet, client) {
-    console.log('WOOOOOW')
-    console.log(client)
+  console.log('something subscribed')
+})
+
+aedes.on('closed', async function (packet, client) {
+  console.log("I lost some guy")
 })
